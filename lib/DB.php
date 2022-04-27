2e6eabd0b42a28525a73d35832d35040b9a22a23
l <javiersettex@gmail.com>
2022-04-26 22:46:46 -0500

    conection test 1

--- lib/DB.php
+++ lib/DB.php
@@ -1,7 +1,7 @@
 <?php
 	class conn extends mysqli{
 		function __construct(){
-			parent::__construct("localhost","portrans_root","Password.2022+","portrans_data");
+			parent::__construct("localhost","u931227564_pucunoa","Password.2022+","u931227564_pucunoa");
 			if (mysqli_connect_error()) {
 				print("error de conexion");
 			}

cec556022ff6c78cbaff96ba558fc010308308e1
l <javiersettex@gmail.com>
2022-04-26 23:02:39 -0500

    test 3

--- app/rest/index.php
+++ app/rest/index.php
@@ -1,12 +1,9 @@
 <?php 
-  header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-  header('content-type: application/json; charset=utf-8');
+  require "header.php";
   $obj=new conn;
   http_response_code(200);
   $return['success']="true";
-  $return['message']="Esta este es el index";
+  $return['message']="Este es el index";
   $return['sql']=null;
   $return['num_res']=null;
   $return['data']=null;
--- app/rest/vawer.php
+++ app/rest/vawer.php
@@ -1,1 +1,11 @@
-hola soy vawer
+<?php 
+  require "header.php";
+  $obj=new conn;
+  http_response_code(200);
+  $return['success']="true";
+  $return['message']="Soy vawer";
+  $return['sql']=null;
+  $return['num_res']=null;
+  $return['data']=null;
+  echo json_encode($return);
+?>
